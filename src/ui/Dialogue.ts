afb3072edd1a71ea38d75021ae4a17a372838b14
stovenly <stovenly@gmail.com>
2026-09-03 19:54:03 -0400

    Take the quest mark off dialogue choices

--- src/ui/Dialogue.ts
+++ src/ui/Dialogue.ts
@@ -259,7 +259,7 @@ export class Dialogue {
         topic.chosen?.();
         this.say(topic.reply);
       });
-      if (topic.quest) choice.classList.add('is-quest', 'quest-mark');
+      if (topic.quest) choice.classList.add('is-quest');
       this.choicesEl.append(choice);
     }
     // Always there, always last, and set apart: it is the way out rather than
